240545d95a5d159f6c6d3feb8e54831f4eec6ab2
JairoDev <jairobernaldev@gmail.com>
2021-09-22 12:31:27 +0200

    Cambio de libreria

--- apiConnections.py
+++ apiConnections.py
@@ -1,30 +1,36 @@
 import requests, json
 from yahoo_finance import Share, Currency
+import yfinance as yf
 
 class ApiConnections:
 
     # Atributos
     __YAHOO_SHARE = "" # Acciones
-    __YAHOO_CURRENCY = "" # Divisas
 
     # Constructor
     def __init__(self):
+        self.__YAHOO_SHARE = yf.Ticker("MSFT")
         print("Constructor en desarrollo")
 
     # Funciones Getters
     def getYahooShare(self):
         return self.__YAHOO_SHARE
     
-    def getYahooCurrency(self):
-        return self.__YAHOO_CURRENCY
-    
     # Funciones Setters
     def setYahooShare(self, share):
-        self.__YAHOO_SHARE = share
+        self.__YAHOO_SHARE = yf.Ticker(share)
         print(f"Configuración cambiada correctamente a {self.__YAHOO_SHARE}")
     
-    def setYahooCurrency(self, currency):
-        self.__YAHOO_CURRENCY = currency
-        print(f"Configuración cambiada correctamente a {self.__YAHOO_CURRENCY}")
 
-    
+    # Funciones
+    """
+    getYahooHistorical() Devuelve los precios de del activo en el rango de fechas que
+    se les pase por parametros.
+
+    # tempI: Fecha de inicio (EJ: '2014-04-25')
+    # tempF: Fecha de finalización (EJ: '2014-04-29')
+    """
+
+    def getYahooHistorical(self, tempI, tempF):
+        data = self.__YAHOO_SHARE.get_historical(tempI, tempF)
+        return data
--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from apiConnections import ApiConnections
 
 api = ApiConnections()
 
-api.setYahooShare("YHOO")
-api.setYahooCurrency("EURUSD")
-print(api.getYahooCurrency())
-print(api.getYahooShare())
+# api.setYahooShare("YHOO")
+# api.setYahooCurrency("EURUSD")
+# print(api.getYahooCurrency())
+# print(api.getYahooShare())
+
+# api.getYahooHistorical('2014-04-25', '2014-04-29')
