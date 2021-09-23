0da12fb3d6e6c461f6f7b4ce7985c7b05dc6afd6
JairoDev <jairobernaldev@gmail.com>
2021-09-23 11:44:19 +0200

    Api de yahoo finalizada

--- apiConnections.py
+++ apiConnections.py
@@ -6,12 +6,14 @@ import pandas as pd
 class ApiConnections:
 
     # Atributos
-    __YAHOO_SHARE = "" # Acciones
+    __YAHOO_SHARE = ""
+    __MULTIPLE_SHARE = []
     __SHARE = ""
 
     # Constructor
     def __init__(self):
         self.__YAHOO_SHARE = yf.Ticker("MSFT")
+        self.__MULTIPLE_SHARE = ["SPY", "AAPL", "MSFT"]
         self.__SHARE = "MSFT"
         print("Constructor en desarrollo")
 
@@ -19,16 +21,23 @@ class ApiConnections:
     def getYahooShare(self):
         return self.__SHARE
     
+    def getYahooShareMultiple(self):
+        return self.__MULTIPLE_SHARE
+    
     # Funciones Setters
     def setYahooShare(self, share):
         self.__YAHOO_SHARE = yf.Ticker(share)
         self.__SHARE = share
         print(f"Configuración cambiada correctamente a {self.__SHARE}")
     
+    def setYahooShareMultiple(self, shares):
+        self.__MULTIPLE_SHARE = shares
+        print(f"Configuración cambiada correctamente a {self.__MULTIPLE_SHARE}")
+    
 
     # Funciones
     """
-    getYahooHistorical() Devuelve los precios de del activo en el rango de fechas que
+    getYahooHistorical() Devuelve los precios del activo en el periodo que
     se les pase por parametros.
 
     # period: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
@@ -49,3 +58,19 @@ class ApiConnections:
         jsonData["price"] = price
         
         return jsonData
+    
+    """
+    getMultipleHistorical() Devuelve los precios de los activos en el periodo que
+    se les pase por parametros.
+
+    # period: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+    """
+    def getMultipleHistorical(self, period):
+        data = []
+
+        for share in self.__MULTIPLE_SHARE:
+            self.setYahooShare(share)
+            data.append(self.getYahooHistorical(period))
+        
+        return data
+
--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from apiConnections import ApiConnections
 
 api = ApiConnections()
 
-data = api.getYahooHistorical("1mo")
+# data = api.getYahooHistorical("1mo")
+data = api.getMultipleHistorical("1mo")
 print(data)
