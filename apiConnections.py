2c062db2456d80101c37f38913ba5e2c42adae76
JairoDev <jairobernaldev@gmail.com>
2021-09-23 12:06:55 +0200

    Error 404 Servidores caidos

--- apiConnections.py
+++ apiConnections.py
@@ -1,5 +1,5 @@
 import requests, json
-from yahoo_finance import Share, Currency
+from googlefinance import getQuotes
 import yfinance as yf
 import pandas as pd
 
@@ -7,12 +7,14 @@ class ApiConnections:
 
     # Atributos
     __YAHOO_SHARE = ""
+    __GOOGLE_SHARE = ""
     __MULTIPLE_SHARE = []
     __SHARE = ""
 
     # Constructor
     def __init__(self):
         self.__YAHOO_SHARE = yf.Ticker("MSFT")
+        self.__GOOGLE_SHARE = "AAPL"
         self.__MULTIPLE_SHARE = ["SPY", "AAPL", "MSFT"]
         self.__SHARE = "MSFT"
         print("Constructor en desarrollo")
@@ -21,7 +23,10 @@ class ApiConnections:
     def getYahooShare(self):
         return self.__SHARE
     
-    def getYahooShareMultiple(self):
+    def getGoogleShare(self):
+        return self.__GOOGLE_SHARE
+
+    def getShareMultiple(self):
         return self.__MULTIPLE_SHARE
     
     # Funciones Setters
@@ -30,7 +35,11 @@ class ApiConnections:
         self.__SHARE = share
         print(f"Configuración cambiada correctamente a {self.__SHARE}")
     
-    def setYahooShareMultiple(self, shares):
+    def setGoogleShare(self, share):
+        self.__GOOGLE_SHARE = share
+        print(f"Configuración cambiada correctamente a {self.__GOOGLE_SHARE}")
+    
+    def setShareMultiple(self, shares):
         self.__MULTIPLE_SHARE = shares
         print(f"Configuración cambiada correctamente a {self.__MULTIPLE_SHARE}")
     
@@ -59,6 +68,15 @@ class ApiConnections:
         
         return jsonData
     
+    """
+    getGoogleData() Devuelve los precios del activo en el periodo que
+    se les pase por parametros.
+
+    # period: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+    """
+    def getGoogleData(self):
+        return json.dumps(getQuotes(self.__GOOGLE_SHARE), indent=2)
+
     """
     getMultipleHistorical() Devuelve los precios de los activos en el periodo que
     se les pase por parametros.
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+#Api V1.1
 from apiConnections import ApiConnections
 
 api = ApiConnections()
 
+"""
+Funciones de yahoo finance
+"""
 # data = api.getYahooHistorical("1mo")
-data = api.getMultipleHistorical("1mo")
-print(data)
+# data = api.getMultipleHistorical("1mo")
+# print(data)
+
+"""
+Funciones de google finance
+"""
+print(api.getGoogleData())
